6d5555ed8ca1c496d858418e1ab91a0f60bea249
alexinfanger <13140491+alexinfanger@users.noreply.github.com>
2023-10-14 17:39:33 -0400

    fixed typo in bash script running loss calculations

--- reverse-llm-benchmarking/run_loss_calculations.sh
+++ reverse-llm-benchmarking/run_loss_calculations.sh
@@ -2,4 +2,4 @@
 
 python stationary_reversal_loss.py --samples 1000 --suffix_batch_size 1 --sample_length 50 --vocab_batch_size 262 
 python forward_llm_loss.py --samples 1000 --batch_size 1 --sample_length 50
-python reverse_llm_loss.py --samples 1000 --batch_size 1 --sample_lenth 50
+python reverse_llm_loss.py --samples 1000 --batch_size 1 --sample_length 50
