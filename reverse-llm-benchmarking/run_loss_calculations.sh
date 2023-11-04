1d071d463bf279df480d52e2728c2ba0d45f37d8
alexinfanger <13140491+alexinfanger@users.noreply.github.com>
2023-11-04 01:01:09 -0400

    run loss calculations typo fixed

--- reverse-llm-benchmarking/run_loss_calculations.sh
+++ reverse-llm-benchmarking/run_loss_calculations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --dist ../data/pile10k_empirical.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.2 --dist ../data/pile10k_empirical.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.4 --dist ../data/pile10k_empirical.pt
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --dist ../data/distributions/pile10k_empirical.pt
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.2 --dist ../data/distributions/pile10k_empirical.pt
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.4 --dist ../data/distributions/pile10k_empirical.pt
