d3a60641737bbc8c24fc87f3d9d56de0847e9776
alexinfanger <13140491+alexinfanger@users.noreply.github.com>
2023-11-04 09:43:13 -0400

    run buffer experiments redo

--- reverse-llm-benchmarking/run_loss_calculations.sh
+++ reverse-llm-benchmarking/run_loss_calculations.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576  --dist ../data/distributions/pile10k_empirical.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576 --dilution 0.2 --dist ../data/distributions/pile10k_empirical.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576 --dilution 0.4 --dist ../data/distributions/pile10k_empirical.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576 --dilution 0.2 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576 --dilution 0.4 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 12576 --dilution 1.0 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --dist ../data/distributions/pile10k_empirical.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.2 --dist ../data/distributions/pile10k_empirical.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.4 --dist ../data/distributions/pile10k_empirical.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.2 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.4 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 1.0 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 10 --dist ../data/distributions/pile10k_empirical.pt
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 20 --dist ../data/distributions/pile10k_empirical.pt
+
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 10 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 20 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+
 
 
 
