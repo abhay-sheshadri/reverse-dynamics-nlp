8cb7f28c7c2b12d6f4b366bb73a3fb4afecce182
alexinfanger <13140491+alexinfanger@users.noreply.github.com>
2023-11-05 16:10:45 -0500

    run marginals

--- reverse-llm-benchmarking/run_loss_calculations.sh
+++ reverse-llm-benchmarking/run_loss_calculations.sh
@@ -8,11 +8,20 @@
 # python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 0.4 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
 # python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288 --dilution 1.0 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
 
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 10 --dist ../data/distributions/pile10k_empirical.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 20 --dist ../data/distributions/pile10k_empirical.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 10 --dist ../data/distributions/pile10k_empirical.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 20 --dist ../data/distributions/pile10k_empirical.pt
 
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 10 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
-python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 20 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 10 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+# python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 20 --dist ../data/distributions/pythia-160m-deduped-v0_stationary_dist.pt
+
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 0 --dilution 0.0 --dist ../data/distributions/probs_10.pt --multiple_priors_start_idx 0 --multiple_priors_end_idx 9
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 0 --dilution 0.2 --dist ../data/distributions/probs_10.pt --multiple_priors_start_idx 0 --multiple_priors_end_idx 9
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 0 --dilution 0.4 --dist ../data/distributions/probs_10.pt --multiple_priors_start_idx 0 --multiple_priors_end_idx 9
+
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 0 --dilution 0.0 --dist ../data/distributions/smoothed_probs_10.pt --multiple_priors_start_idx 0 --multiple_priors_end_idx 9
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 0 --dilution 0.2 --dist ../data/distributions/smoothed_probs_10.pt --multiple_priors_start_idx 0 --multiple_priors_end_idx 9
+python stationary_reversal_loss.py --num_examples 100 --vocab_batch_size 6288  --num_buffer 0 --dilution 0.4 --dist ../data/distributions/smoothed_probs_10.pt --multiple_priors_start_idx 0 --multiple_priors_end_idx 9
+
 
 
 
