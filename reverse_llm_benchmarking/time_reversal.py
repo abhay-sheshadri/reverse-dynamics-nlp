cf47a910c9aade9c3bf1333e74c7a6a193c5fd6d
alexinfanger <13140491+alexinfanger@users.noreply.github.com>
2023-11-15 18:01:40 -0500

    timed reversal

--- reverse_llm_benchmarking/time_reversal.py
+++ reverse_llm_benchmarking/time_reversal.py
@@ -1,5 +1,14 @@
+import numpy as np
+from tqdm import tqdm
+import time
+import math
+import gc
+import torch
+from datasets import load_dataset
+from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast, DataCollatorForLanguageModeling
 from reverse_sampling import sample_reverse_dynamics_reverse_prior
 import time
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = GPTNeoXForCausalLM.from_pretrained(
     "EleutherAI/pythia-12B-deduped",
@@ -10,27 +19,9 @@ reverse_model = GPTNeoXForCausalLM.from_pretrained(
     "afterless/reverse-pythia-160m"
 ).to(device)
 
-tokenized_suffix = tokenizer.encode(" Clinton")
-
-
 # current time
-start = time.time()
-out = sample_reverse_dynamics_reverse_prior(
-    model,
-    reverse_model,
-    10,
-    tokenized_suffix,
-    vocab_batch_size=3000,
-    temperature=1.0,
-    dilution=0.0,
-    device="cuda"
-)
-print(out)
-end = time.time()
-print(end - start)
 tokenized_suffix = tokenizer.encode(" basketball")
-
-
+print(tokenized_suffix.shape)
 # current time
 start = time.time()
 out = sample_reverse_dynamics_reverse_prior(
