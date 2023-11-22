92a0a38afc805e8515bda3b76578d465460cca43
alexinfanger <13140491+alexinfanger@users.noreply.github.com>
2023-11-22 17:05:39 -0500

    put tensors on cpu before pickle

--- reversal_suffix_elicitation.py
+++ reversal_suffix_elicitation.py
@@ -77,8 +77,8 @@ def main():
         
         output_stats[suffix] = {
             "gt_prefix": prefix,
-            "gt_prefix_loss": prefix_loss,
-            "gt_suffix_loss": suffix_loss,
+            "gt_prefix_loss": prefix_loss.to("cpu"),
+            "gt_suffix_loss": suffix_loss.to("cpu"),
             "prompt_opts": {
                 opt: {} for opt in optimizers
             }
