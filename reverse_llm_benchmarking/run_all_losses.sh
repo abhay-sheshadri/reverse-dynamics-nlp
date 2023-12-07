2ec2b813cd73d8eb6cc7f93f98c3a4d63d1c6953
alexinfanger <alexdinfanger@gmail.com>
2023-12-07 02:04:13 +0000

    big run on A100 to double check old run

--- reverse_llm_benchmarking/run_all_losses.sh
+++ reverse_llm_benchmarking/run_all_losses.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-prefix_length=19
+prefix_length=29
 suffix_length=1
 num_examples=100
-filename_prefix=al_12_6_
+filename_prefix=al_12_6_to_check_old_run_
 dataset_name=pile_val
 seed=5491
 device=cuda
 full_data_set_chunk=false
-vocab_batch_size=500
+vocab_batch_size=1000
 
 # Reversal
 python all_methods_loss.py --num_examples $num_examples --prefix_length $prefix_length --reverse_model_prior true --model_size "160m" --filename_prefix $filename_prefix  --dataset_name $dataset_name --seed $seed --device $device --full_data_set_chunk $full_data_set_chunk --vocab_batch_size $vocab_batch_size
