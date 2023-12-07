0b2d654402eba0b21d5656559e264fb0d9451d1f
alexinfanger <alexdinfanger@gmail.com>
2023-12-07 00:06:17 +0000

    reduce vocab batch size

--- reverse_llm_benchmarking/run_all_losses.sh
+++ reverse_llm_benchmarking/run_all_losses.sh
@@ -7,6 +7,7 @@ dataset_name=pile_val
 seed=5491
 device=cuda
 full_data_set_chunk=false
+vocab_batch_size=500
 
 # Reversal
-python all_methods_loss.py --num_examples $num_examples --prefix_length $prefix_length --reverse_model_prior true --model_size "160m" --filename_prefix $filename_prefix  --dataset_name $dataset_name --seed $seed --device $device --full_data_set_chunk $full_data_set_chunk --vocab_batch_size 2000
+python all_methods_loss.py --num_examples $num_examples --prefix_length $prefix_length --reverse_model_prior true --model_size "160m" --filename_prefix $filename_prefix  --dataset_name $dataset_name --seed $seed --device $device --full_data_set_chunk $full_data_set_chunk --vocab_batch_size $vocab_batch_size
