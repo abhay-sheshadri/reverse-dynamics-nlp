5fe4ef5fb41bef9626a85287197eaa38dc15c32e
alexinfanger <alexdinfanger@gmail.com>
2023-12-07 02:20:58 +0000

    new suffix elicitation run

--- run_suffix_elicitation.sh
+++ run_suffix_elicitation.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-date
-python reversal_suffix_elicitation.py --model_size 160m --eval_size 250 --dataset pile_val --vocab_batch_size 1000
-date
-python reversal_suffix_elicitation.py --model_size 160m --eval_size 250 --dataset allenai/real-toxicity-prompts --vocab_batch_size 1000
-date
+python reversal_suffix_elicitation.py --model_size 160m --eval_size 100 --dataset pile_val --vocab_batch_size 1000
 
-
-
-#python reversal_suffix_elicitation.py --model_size 1.4B --eval_size 50
-#python reversal_suffix_elicitation.py --model_size 2.8B --eval_size 50
-
-#date
-#python reversal_suffix_elicitation.py --model_size 1B --eval_size 50
-#date
-# old run
-#date
-#python reversal_suffix_elicitation.py --model_size 410m --eval_size 10
-#date
-#python reversal_suffix_elicitation.py --model_size 410m --eval_size 50
-#date
-#python reversal_suffix_elicitation.py --model_size 160m --eval_size 50
-#date
---
+++ run_suffix_elicitation_2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python reversal_suffix_elicitation.py --model_size 160m --eval_size 100 --dataset allenai/real-toxicity-prompts --vocab_batch_size 1000
