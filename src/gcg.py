abaa7a3918eb9e5992ed48101a1fb8ade5081b77
Jacob Pfau <jp6263@log-1.hpc.nyu.edu>
2024-02-29 17:07:56 -0500

    fixed gcg default hp

--- src/gcg.py
+++ src/gcg.py
@@ -83,7 +83,7 @@ class GreedyCoordinateGradient:
         self,
         model: AutoModelForCausalLM,
         tokenizer: AutoTokenizer,
-        n_proposals: int = 128,
+        n_proposals: int = 512,
         n_epochs: int = 500,
         n_top_indices: int = 256,
         prefix_loss_weight: float = 0.0
