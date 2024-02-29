148b5ade64dc488f99f7226fedb36c93ea142530
Jacob Pfau <jp6263@log-1.hpc.nyu.edu>
2024-02-29 18:16:19 -0500

    fixed dropping of gcg thing

--- src/gcg.py
+++ src/gcg.py
@@ -146,7 +146,7 @@ class GreedyCoordinateGradient:
                                         truncation=True, 
                                         max_length=input_slice.stop - input_slice.start,
                                     )['input_ids']
-        proposals = proposal[1:] # Drop the previous input--first proposal
+        proposals = proposals[1:] # Drop the previous input--first proposal
         proposals[:,input_slice] = candidates[1:] 
         return proposals
 
